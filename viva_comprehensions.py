9f5afa5de226216446499d4c36bba63533230a03
JLAT <jlat0128@gmail.com>
2020-03-24 11:58:45 -0400

    corrected spelling and docustrings

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -11,7 +11,7 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     """
     :param start: Where to start the list (inclusive).
     :param stop: Where to stop the list (exclusive).
-    :param parity: Based on which parity is passed, the method will return even or odd list
+    :param parity: Based on which parity is passed, the method will be applied to each item in the list
     :return: Returns a list in specified range parity
     """
     if parity == Parity.ODD:
@@ -27,9 +27,9 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     """
-    :param start: Where to start the list (inclusive).
-    :param stop: Where to stop the list (exclusive).
-    :param strategy: takes in a lamda function to carryout on each item in the list
+    :param start: Key value start (inclusive).
+    :param stop: Key value end (exclusive).
+    :param strategy: takes in a lambda function to carryout on each item in the list
     :return: Returns a dict with keys in range, and a value with a strategy applied for each key value
     """
     dicts = {}
@@ -42,7 +42,7 @@ def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
 def gen_set(val_in: str) -> set:
     """
     :param val_in: Takes in a string
-    :return: Returns a set of only lower case distant characters from the string, and returns them capitalized in the set.
+    :return: Returns a set of only lower case distanct characters from the string, and returns them capitalized in the set.
     """
     
     lst =list(val_in)
